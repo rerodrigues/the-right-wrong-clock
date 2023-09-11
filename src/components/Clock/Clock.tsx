d4da1cb43e05d65c78a8fae3e42d6e5cf2dd0d6a
Renato Rodrigues <renato.sp@gmail.com>
2023-09-12 01:07:02 +0200

    style: square digital clock

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,18 +1,40 @@
 import './Clock.css';
-import { getHours, getMinutes } from 'date-fns';
-import { memo } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import { ClockProps } from '../../types/Clock';
+import { ONE_SECOND_IN_MS } from '../../constants';
 
 const Clock = memo((props: ClockProps) => {
   const { ownTime, isEnabled } = props;
+  const [time, setTime] = useState<Date>(new Date(ownTime));
+  const timerId = useRef<NodeJS.Timer>();
+
   const className = isEnabled ? 'clock clock--lit' : 'clock';
-  const hour = getHours(ownTime).toString().padStart(2, '0');
-  const minute = getMinutes(ownTime).toString().padStart(2, '0');
+  const hour = time.getHours().toString().padStart(2, '0');
+  const minute = time.getMinutes().toString().padStart(2, '0');
+  const seconds = time.getSeconds().toString().padStart(2, '0');
+
+  useEffect(() => {
+    if(isEnabled) {
+      const intervalFn = () => {
+        setTime(new Date());
+      };
+
+      setTime(new Date());
+      timerId.current = setInterval(intervalFn, ONE_SECOND_IN_MS);
+
+      return () => {
+        if(timerId.current) clearTimeout(timerId.current);
+      }
+    }
+  },[isEnabled, ownTime]);
 
   return (
-    <div className={className}>
-      <div className='clock_hour'>{hour}</div>
-      <div className='clock_minute'>{minute}</div>
+    <div className="clock-container">
+      <div className={className}>
+        <span className='clock_hour'>{hour}</span>
+        <span className='clock_minute'>{minute}</span>
+        <span className='clock_seconds'>{seconds}</span>
+      </div>
     </div>
   );
 });
