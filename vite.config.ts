5243fd0b931b58a6fd273d42931a994644205a15
Renato Rodrigues <renato.sp@gmail.com>
2023-09-21 23:47:20 +0200

    chore: vite config tunning

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const root = path.resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root,
+  publicDir: path.resolve(root, '../public'),
+  build: {
+    outDir: path.resolve(root, '../dist'),
+    emptyOutDir: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 4000,
+    open: true,
+  },
 });
